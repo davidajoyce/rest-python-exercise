6d6a965675edd2efd77c9b99b24023b2d93787b4
David PC <davidajoyce141@gmail.com>
2017-09-24 17:40:05 +0100

    testing the get request function and data from get request

--- rest-python.py
+++ rest-python.py
@@ -15,10 +15,26 @@ def test_file_empty(input_file):
 def error_message_file_empty():
 	sys.stderr.write('input file has no URLs to check')
 
+#testing the get request to make sure receiving correct information
+def test_get_request(input_file_line):
 
+	request = get_request(input_file_line)
+	status_code = get_status_code(request)
+	content_length = get_content_length(request)
+	Date = date_of_request(request)
+	
+	test_status_code(status_code)
+	test_content_length(content_length)
+	test_date(Date)
+
+
+#main function for tests
 def main():
 	input_file = sys.stdin.readlines()
 	test_stdin_file(input_file)
+	for line in input_file:
+		test_get_request(line)
+
 
 main()
 	
