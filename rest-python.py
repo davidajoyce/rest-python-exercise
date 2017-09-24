693183c7864f6696681d4ee79134813b910ba4ef
David PC <davidajoyce141@gmail.com>
2017-09-24 17:28:31 +0100

    commit where initial test for input file passes

--- rest-python.py
+++ rest-python.py
@@ -2,9 +2,18 @@ import sys
 
 #test to make sure file contains a string to test for URL
 def test_stdin_file(input_file):
-	file_empty = test_file_not_empty()
+	file_empty = test_file_empty(input_file)
 	if file_empty:
-		error_message()
+		error_message_file_empty()
+
+#check input file is not empty
+def test_file_empty(input_file):
+	if len(input_file) == 0:
+		return True
+
+# output error message to sterr
+def error_message_file_empty():
+	sys.stderr.write('input file has no URLs to check')
 
 
 def main():
