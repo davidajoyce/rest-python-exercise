1b624a80a9cd586505cd44019bd1d7bc3e4bd185
David PC <davidajoyce141@gmail.com>
2017-09-24 18:08:10 +0100

    test to make sure can perform get request included

--- rest-python.py
+++ rest-python.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 
 #test to make sure file contains a string to test for URL
 def test_stdin_file(input_file):
@@ -15,6 +16,11 @@ def test_file_empty(input_file):
 def error_message_file_empty():
 	sys.stderr.write('input file has no URLs to check')
 
+def test_URL(URL_check):
+	check_http_s = check_https(URL_check)
+	if not check_hhtp_s
+		error_message_notURL()
+
 #testing the get request to make sure receiving correct information
 def test_get_request(input_file_line):
 
@@ -27,13 +33,28 @@ def test_get_request(input_file_line):
 	test_content_length(content_length)
 	test_date(Date)
 
+def get_request(input_file_line):
+	request_get = requests.get(input_file_line)
+	print request_get.content
+	return request_get
+
+def get_status_code(request):
+	status_code = request.status_code
+	print status_code
+	return status_code
+
 
 #main function for tests
 def main():
 	input_file = sys.stdin.readlines()
 	test_stdin_file(input_file)
+
 	for line in input_file:
-		test_get_request(line)
+		URL = line.strip()
+		test_URL()		
+		test_get_request(URL)
+
+
 
 
 main()
