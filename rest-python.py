5375902290f7a33e913007bcc746ccf73417b3c8
David PC <davidajoyce141@gmail.com>
2017-09-24 18:51:53 +0100

    executing the get request to retrieve the required information from the header

--- rest-python.py
+++ rest-python.py
@@ -18,7 +18,7 @@ def error_message_file_empty():
 
 def test_URL(URL_check):
 	check_http_s = check_https(URL_check)
-	if not check_hhtp_s
+	if not check_hhtp_s:
 		error_message_notURL()
 
 #testing the get request to make sure receiving correct information
@@ -27,21 +27,31 @@ def test_get_request(input_file_line):
 	request = get_request(input_file_line)
 	status_code = get_status_code(request)
 	content_length = get_content_length(request)
-	Date = date_of_request(request)
+	Date = date_of_response(request)
 	
-	test_status_code(status_code)
-	test_content_length(content_length)
-	test_date(Date)
+	#test_status_code(status_code)
+	#test_content_length(content_length)
+	#test_date(Date)
 
 def get_request(input_file_line):
+
 	request_get = requests.get(input_file_line)
-	print request_get.content
 	return request_get
 
 def get_status_code(request):
 	status_code = request.status_code
-	print status_code
 	return status_code
+
+def get_content_length(request):
+
+	header = request.headers
+	content_length = header.get('Content-length')
+	return content_length
+	
+def date_of_response(request):
+	header = request.headers
+	date = header.get('Date')
+	return date
 
 
 #main function for tests
@@ -51,7 +61,7 @@ def main():
 
 	for line in input_file:
 		URL = line.strip()
-		test_URL()		
+		#test_URL(URL)		
 		test_get_request(URL)
 
 
